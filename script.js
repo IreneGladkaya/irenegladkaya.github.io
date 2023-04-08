dd3cbb422ebc38ce6aabb38da48d50fce0abd1c2
IrinaBezgina <gladkayairene@gmail.com>
2023-04-08 15:57:27 +0100

    sdebar menu

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const body = document.querySelector("body"),
     sidebar = body.querySelector(".sidebar"),
-    toggle = body.querySelector(".toggle"),
-    searchBtn = body.querySelector(".search-box");
+    toggle = body.querySelector(".toggle");
 
 toggle.addEventListener("click", () =>{
     sidebar.classList.toggle("close");
